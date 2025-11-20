d87372c41fe012f0e0baf289b68690d93bd14a7f
Vasu Behl <vasubehl8@gmail.com>
2025-11-20 12:58:51 +0530

    Remove dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Remove the GitHub Pages base – Vercel serves from "/"
-  // base: "/staywithMVA/",
 });
