732a70ebca7eaa40d4604610f8f0e7acb8928605
Vasu Behl <vasubehl8@gmail.com>
2025-11-20 13:23:30 +0530

    Fix router and file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import Pg from "./pages/Pg";
-import Contactus from "./pages/Contactus";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
-// 👇 Utility to always scroll to top on route change
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  }, [pathname]);
-
-  return null;
-}
+import Home from "./pages/Home.jsx";
+import Pg from "./pages/Pg.jsx";
+import Rooms from "./pages/Rooms.jsx";
+import Contactus from "./pages/Contactus.jsx";
 
 function App() {
   return (
-    <Router basename="/staywithMVA">
-      <ScrollToTop /> {/* ensures every route starts at the top */}
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms />} />
         <Route path="/pg" element={<Pg />} />
+        <Route path="/rooms" element={<Rooms />} />
         <Route path="/contact" element={<Contactus />} />
       </Routes>
     </Router>
